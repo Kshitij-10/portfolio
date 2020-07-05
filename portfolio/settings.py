908717355751677a6c265c9a6e47c81c6eeca7de
Kshitij-10 <Kshitij-10@users.noreply.github.com>
2020-07-05 13:30:44 +0530

    f

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -143,3 +143,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
